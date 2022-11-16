31d871b3e509be0c8e2daa376d6c5941369a3eb8
Amit Martinez <amitmartinez50@gmail,com>
2022-11-16 08:21:25 -0300

    Actualizacion de formularios y lista de usuario 80%

--- django_init/usuario/urls.py
+++ django_init/usuario/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import add_user, soporte, home, list_user, edit_user
+from .views import add_user, soporte, home, list_user, edit_user, delete_user
 
 urlpatterns = [
     path('', home, name='home'),
     path('add_user/', add_user, name='add_user'),
     path('list_user/', list_user, name='list_user'),
-    path('edit_user/', edit_user, name='edit_user'),
+    path('edit_user/<int:pk>', edit_user, name='edit_user'),
+    path('delete_user/<int:pk>', delete_user, name='delete_user'),
     path('soporte/', soporte, name='soporte'),
 ]
--- django_init/usuario/views.py
+++ django_init/usuario/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Job, Employee
 from .forms import EmployeeForm
+
 # Create your views here.
 
 j = Job()
@@ -10,9 +11,8 @@ j = Job()
 def soporte(request):
     return render(request, 'registration/soporte.html')
 
+
 # CRUD Usuarios
-
-
 @login_required
 def home(request):
     return render(request, 'usuario/home.html')
@@ -30,18 +30,32 @@ def add_user(request):
         else:
             data["form"] = form
     return render(request, 'usuario/add_user.html', data)
+
+
 @login_required
-def edit_user(request):
-    data = {
-        'form': EmployeeForm()
-    }
-    if request.method == 'PUT':
-        form = EmployeeForm(data=request.PUT)
+def edit_user(request, pk):
+    employee = Employee.objects.get(id=pk)
+    form = EmployeeForm(instance=employee)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
-        else:
-            data["form"] = form
-    return render(request,'usuario/edit_user.html', data)
+            return redirect('/list_user/')
+    elif request.method == 'PUT':
+        form = EmployeeForm(request.PUT, instance=employee)
+    data = {'form': form}
+    return render(request, 'usuario/edit_user.html', data)
+
+@login_required
+def delete_user(request, pk):
+    employee = Employee.objects.get(id=pk)
+    if request.method == "POST":
+        employee.delete()
+        return redirect('/list_user/')
+    data = {'employee': employee}
+    return render(request, 'usuario/delete_user.html', data)
+
+
 @login_required
 def list_user(request):
     employee = Employee.objects.all()
